302dbc7934ac7065f0454ad8b47416af735572b8
Eli Fatsi <elias.fatsi.24@gmail.com>
2023-02-28 15:21:00 -0500

    Setup frontend docker service for running vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config https://vitest.dev/config
+// https://vitejs.dev/config
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: '.vitest/setup',
-    include: ['**/test.{ts,tsx}']
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
   }
 })
