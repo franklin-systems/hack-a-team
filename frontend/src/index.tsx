21340004049ebbc875aee096bc75d8c066b05c82
Eli Fatsi <elias.fatsi.24@gmail.com>
2023-03-02 20:41:13 -0700

    Set up react router, with one Home page

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
-import App from 'components/App'
+
+import Router from 'router'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
-root.render(<App />)
+root.render(<Router />)
