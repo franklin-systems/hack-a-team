5a7853bde4a251eb17ecc4db0215928c1f42e872
Nikhil Narula <nikhilnarula.2242@gmail.com>
2023-03-04 15:26:11 -0700

    Compose is cool

--- frontend/src/components/happy-buttons.tsx
+++ frontend/src/components/happy-buttons.tsx
@@ -1,19 +1,41 @@
-import { ComposeClient } from '@composedb/client'
-import { definition } from 'schema/runtime-composite.js'
-
-const compose = new ComposeClient({ ceramic: 'http://localhost:7007', definition })
-compose.setDID("did:key:z6MkmoPz6RB1aowAvXWVmEyTMsuKehT2J577nHLY6H1ZzMZU")
+import getComposeClient from "utils/compose";
 
 async function handleClick(event) {
+  const compose = await getComposeClient()
   event.preventDefault();
-  await compose.executeQuery(`
-  `);
+  const queryResponse = await compose.executeQuery(
+    `query{
+      hackathonProfileIndex(first: 10) {
+        edges {
+          node {
+            name
+          }
+        }
+      }
+    }`
+  )
+  console.log(queryResponse)
+  const response = await compose.executeQuery(`
+    mutation CreateNewHackathonProfile($i: CreateHackathonProfileInput!){
+      createHackathonProfile(input: $i){
+        document{
+          id
+          name
+        }
+      }
+    }
+  `, {
+    "i": {
+      "content": {
+        "name": "Random Name " + Math.floor(Math.random() * 10000)
+      }
+    }
+  });
+  console.log(response)
 }
 
 export default function HappyButtons() {
-
-
   return (
-    <button type="button" onClick={handleClick}>Create a random hacker profile!!</button>
+    <button className="rounded-md bg-white py-2 px-3 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" type="button" onClick={handleClick}>Create a random hacker profile!!</button>
   )
 }
--- frontend/src/utils/compose.ts
+++ frontend/src/utils/compose.ts
@@ -2,43 +2,49 @@ import { DIDSession } from 'did-session'
 import type { AuthMethod } from '@didtools/cacao'
 import { EthereumWebAuth, getAccountId } from '@didtools/pkh-ethereum'
 import { ComposeClient }from '@composedb/client'
+import { definition } from 'schema/runtime-composite.js'
+import { ethers } from "ethers";
 
-const ethProvider = undefined
-const addresses = await ethProvider.request({ method: 'eth_requestAccounts' })
-const accountId = await getAccountId(ethProvider, addresses[0])
-const authMethod = await EthereumWebAuth.getAuthMethod(ethProvider, accountId)
-
-const compose = new ComposeClient()
-
-const loadSession = async (authMethod) => {
-    // Check if user session already in storage
-  const sessionStr = localStorage.getItem('didsession')
-  let session
-
-    // If session string available, create a new did-session object
-  if (sessionStr) {
-    session = await DIDSession.fromSession(sessionStr)
+const getComposeClient = async () => {
+  const ethProvider = new ethers.providers.Web3Provider(window.ethereum).provider;
+  const addresses = await ethProvider.request({ method: 'eth_requestAccounts' })
+  const accountId = await getAccountId(ethProvider, addresses[0])
+  console.log(accountId)
+  const authMethod = await EthereumWebAuth.getAuthMethod(ethProvider, accountId)
+  const sessionId = "didsession-" + accountId.address
+  
+  const compose = new ComposeClient({ ceramic: 'http://localhost:7007', definition })
+  
+  const loadSession = async (authMethod, sessionId) => {
+      // Check if user session already in storage
+    const sessionStr = localStorage.getItem(sessionId)
+    let session
+  
+      // If session string available, create a new did-session object
+    if (sessionStr) {
+      session = await DIDSession.fromSession(sessionStr)
+    }
+  
+      // If no session available, create a new user session and store in local storage
+    if (!session || (session.hasSession && session.isExpired)) {
+      session = await DIDSession.authorize(authMethod, { resources: compose.resources})
+      localStorage.setItem(sessionId, session.serialize())
+    }
+  
+    return session
   }
-
-    // If no session available, create a new user session and store in local storage
-  if (!session || (session.hasSession && session.isExpired)) {
-        const session = await DIDSession.authorize(authMethod, { resources: compose.resources})
-    localStorage.setItem('didsession', session.serialize())
-  }
-
-  return session
-}
-
-const session = await loadSession(authMethod)
-compose.setDID(session.did)
-
-// pass ceramic instance where needed, ie glaze
-// ...
-
-// Before mutations, check if a session is still valid, if expired, create new
-if (session.isExpired) {
-  const session = loadSession(authMethod)
+  
+  const session = await loadSession(authMethod, sessionId)
   compose.setDID(session.did)
+  
+  // Before mutations, check if a session is still valid, if expired, create new
+  if (session.isExpired) {
+    const session = loadSession(authMethod, sessionId)
+    compose.setDID(session.did)
+  }
+  
+  // perform mutations, continue to use compose client
+  return compose
 }
 
-// perform mutations, continue to use compose client
+export default getComposeClient
