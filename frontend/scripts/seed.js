c0ebe445f1a324783759da42b7619f7354aede5c
Shaan Savarirayan <shaan.savarirayan@gmail.com>
2023-03-04 12:41:16 -0700

    Add seed script for deploying hackathon contract locally

---
+++ frontend/scripts/seed.js
@@ -0,0 +1,19 @@
+async function main() {
+    const ethers = require('ethers');
+    const { abi, bytecode } = require('../src/compiled-contracts/Hackathon.json');
+    
+    const provider = new ethers.providers.JsonRpcProvider('http://ganache:8545');
+    
+    const deployer_account = new ethers.Wallet(process.env.DEPLOYER_PRIVATE_KEY, provider);
+    
+    const hackathon = new ethers.ContractFactory(abi, bytecode, deployer_account);
+    const startTime = Math.floor(new Date().getTime() / 1000);
+    // 1 week from now
+    const endTime = startTime + 604800;
+
+    const contract = await hackathon.deploy(startTime, endTime);
+    
+    console.log('\nHackathon contract deployed to:', contract.address, '\n');
+}
+
+main();
