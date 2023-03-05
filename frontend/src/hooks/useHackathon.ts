fba491d9ad4932645a7474802e9fdf61f46cd089
Shaan Savarirayan <shaan.savarirayan@gmail.com>
2023-03-04 18:04:23 -0700

    Rename

--- frontend/src/hooks/useHackathon.ts
+++ frontend/src/hooks/useHackathon.ts
@@ -3,7 +3,7 @@ import { Contract} from '@ethersproject/contracts'
 import { Web3Provider } from '@ethersproject/providers'
 
 
-export function useHackthonContract(provider: Web3Provider, account: string | null ): Contract {
+export function useHackthon(provider: Web3Provider, account: string | null ): Contract {
 
     const address = import.meta.env.VITE_HACKATHON_CONTRACT_ADDRESS;
 
